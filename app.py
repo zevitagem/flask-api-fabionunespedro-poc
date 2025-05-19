9d4c3ecc4f2a8f8a042590f24bf38c7ea3fb13f7
Fabio Nunes <fabio.pedro@live.com>
2025-05-19 17:26:49 -0300

    feat: post method created

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
 from flask import Flask, request, jsonify
+from models.games import Game
 
 app = Flask(__name__)
 
-app.route('/create', methods = ['POST'])
+games = []
+
+@app.route('/create', methods = ['POST'])
+def create_game():
+  data = request.json
+  game = Game(data['name'], data['category'], data['year'])
+  games.append(game)
+  return jsonify({'message': 'Game criado com sucesso!', 'game': game.to_dict()}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
   app.run(debug=True)
