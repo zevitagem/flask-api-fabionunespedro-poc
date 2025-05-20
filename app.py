c98ef9ddd5fcce1c9c464c34b4b13a83bbee68da
Fabio Nunes <fabio.pedro@live.com>
2025-05-20 13:31:46 -0300

    feat: added method to list all games

--- app.py
+++ app.py
@@ -12,29 +12,9 @@ def create_game():
   games.append(game)
   return jsonify({'message': 'Game criado com sucesso!', 'game': game.to_dict()}), 201
 
+@app.route('/listall', methods = ['GET'])
+def get_all_games():
+   return jsonify([game.to_dict() for game in games])
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-  app.run(debug=True)
-
