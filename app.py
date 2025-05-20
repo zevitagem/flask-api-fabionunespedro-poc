aa5c7e074e145c9b0be550c48d4b897774d55664
morendev <morenodasilvajoao709@gmail.com>
2025-05-20 14:59:48 -0300

    feat: adiciona implementacao do update

--- app.py
+++ app.py
@@ -1,19 +1,41 @@
 from flask import Flask, request, jsonify
 from models.games import Game
+import os
 
 app = Flask(__name__)
 
 games = []
 
+@app.route('/')
+def get_header():
+  headers = request.headers
+  return dict(request.headers)
+
 @app.route('/create', methods = ['POST'])
 def create_game():
   data = request.json
-  game = Game(data['name'], data['category'], data['year'])
+  game = Game(data['name'] , data['category'], data['year'])
   games.append(game)
+
   return jsonify({'message': 'Game criado com sucesso!', 'game': game.to_dict()}), 201
+
+
+@app.route('/update/<string:id_name>', methods = ['PUT'])
+def update_game(id_name):   
+  for i in range(len(games)):
+    if(games[i].name == id_name):
+      data = request.json 
+      games[i].name = data['name']
+      games[i].category = data['category']
+      games[i].year = data['year']
+
+      return jsonify({'message': 'O Game foi atualizado com sucesso', 'Game': games[i].to_dict()}), 200
+  return jsonify({'message': 'Game não encontrado'}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
