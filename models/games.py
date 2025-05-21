fdbca15ab32e9a30304ec6b893aa8e18188008cf
morendev <morenodasilvajoao709@gmail.com>
2025-05-21 14:31:11 -0300

    feat: persistencia de dados aplicada com o banco de dados offline sql alchemy

--- models/games.py
+++ models/games.py
@@ -1,4 +1,13 @@
-class Game():
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer
+
+db = SQLAlchemy()
+
+class Game(db.Model):
+  name = db.Column(db.String(100), primary_key=True,)
+  category = db.Column(db.String(40), nullable=False)
+  year = db.Column(Integer, nullable=False)
+
   def __init__(self, name, category, year):
     self.name = name
     self.category = category
